9b90a831b5fe08265f960174857bbae03303f8a4
Antonio Roque <antoniocarlos.roque@gmail.com>
2021-02-19 10:52:20 -0300

    Styled the App Bar

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,8 +1,12 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import { makeStyles } from "@material-ui/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+
+import logo from "../../assets/logo.svg";
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -19,15 +23,48 @@ function ElevationScroll(props) {
   });
 }
 
+const useStyles = makeStyles((theme) => ({
+  toolbarMargin: {
+    ...theme.mixins.toolbar,
+    marginBottom: "3em",
+  },
+  logo: {
+    height: "7em",
+  },
+  tabContainer: {
+    marginLeft: "auto",
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: "25px",
+  },
+}));
+
 const Header = () => {
+  const classes = useStyles();
   return (
-    <ElevationScroll>
-      <AppBar position='fixed' color='primary'>
-        <Toolbar>
-          <Typography variant='h3'>Arc Development</Typography>
-        </Toolbar>
-      </AppBar>
-    </ElevationScroll>
+    <React.Fragment>
+      <ElevationScroll>
+        <AppBar position='fixed' color='primary'>
+          <Toolbar disableGutters>
+            <img
+              src={logo}
+              alt='Arc Development Logo'
+              className={classes.logo}
+            />
+            <Tabs className={classes.tabContainer}>
+              <Tab className={classes.tab} label='Home' />
+              <Tab className={classes.tab} label='Services' />
+              <Tab className={classes.tab} label='The Revolution' />
+              <Tab className={classes.tab} label='About Us' />
+              <Tab className={classes.tab} label='Contact Us' />
+            </Tabs>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+      <div className={classes.toolbarMargin} />
+    </React.Fragment>
   );
 };
 
