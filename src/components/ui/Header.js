88a504f7138bf8312852487b728166bac8e189ee
Antonio Roque <antoniocarlos.roque@gmail.com>
2021-02-25 08:09:08 -0300

    Got clicking on tabs routing straight with useEffect hook

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import useScrollTrigger from "@material-ui/core/useScrollTrigger";
@@ -31,10 +31,16 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: "3em",
   },
   logo: {
-    height: "7em",
+    height: "8em",
   },
   tabContainer: {
     marginLeft: "auto",
+  },
+  logoContainer: {
+    padding: 0,
+    "&:hover": {
+      backgroundColor: "transparent",
+    },
   },
   tab: {
     ...theme.typography.tab,
@@ -57,16 +63,41 @@ export default function Header(props) {
   const handleChange = (e, value) => {
     setValue(value);
   };
+
+  useEffect(() => {
+    if (window.location.pathname === "/" && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === "/services" && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === "/revolution" && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === "/about" && value !== 3) {
+      setValue(3);
+    } else if (window.location.pathname === "/contact" && value !== 4) {
+      setValue(4);
+    } else if (window.location.pathname === "/estimate" && value !== 5) {
+      setValue(5);
+    }
+  }, [value]);
+
   return (
     <React.Fragment>
       <ElevationScroll>
         <AppBar position='fixed' color='primary'>
           <Toolbar disableGutters>
-            <img
-              src={logo}
-              alt='Arc Development Logo'
-              className={classes.logo}
-            />
+            <Button
+              className={classes.logoContainer}
+              component={Link}
+              to='/'
+              onClick={() => setValue(0)}
+              disableRipple
+            >
+              <img
+                src={logo}
+                alt='Arc Development Logo'
+                className={classes.logo}
+              />
+            </Button>
             <Tabs
               value={value}
               onChange={handleChange}
